e6a1ba0d3c4ec95d086ed5a3d09c18a13abc150e
RatRatARMY <khang0762412064@outlook.com>
2026-05-21 16:51:45 +0700

    Implemented the game (basic).

--- catch-the-apple/src/main.rs
+++ catch-the-apple/src/main.rs
@@ -1,4 +1,6 @@
+use rand::RngExt;
 use {
+    rand::rng,
     sdl3::{
         event::Event,
         keyboard::Keycode,
@@ -10,6 +12,7 @@ use {
     std::{time::Duration, thread::sleep}
 };
 fn main() {
+    let mut random = rng();
     let (width, height): (u32, u32) = image_dimensions("assets/bg_image.png").unwrap();
     let sdl3_context = sdl3::init().unwrap();
     let video_subsystem = sdl3_context.video().unwrap();
@@ -21,9 +24,17 @@ fn main() {
     let texture_creator = canvas.texture_creator();
     let bg_img = texture_creator.create_texture_from_surface(Surface::from_file("assets/bg_image.png").unwrap()).unwrap();
     let cat_surface = Surface::from_file("assets/spr_cat.png").unwrap();
+    let apple_surface = Surface::from_file("assets/spr_apple.png").unwrap();
     let cat_img = texture_creator.create_texture_from_surface(&cat_surface).unwrap();
+    let apple_img = texture_creator.create_texture_from_surface(&apple_surface).unwrap();
     let (mut cat_x, cat_y): (i32, i32) = (0, height as i32 - cat_surface.height() as i32);
+    let (mut apple_x, mut apple_y): (i32, i32) = (random.random_range(0..=width as i32 - apple_surface.width() as i32), 0);
     let mut cat_speed = 0;
+    let apple_speed = 5;
+    let mut time = 120 * 60;
+    let mut score = 0;
+    let mut can_collide = true;
+    let mut game_over = false;
     canvas.present();
     let mut events = sdl3_context.event_pump().unwrap();
     'game_loop: loop {
@@ -47,14 +58,33 @@ fn main() {
                 _ => {}
             }
         }
-        cat_x += cat_speed;
-        if cat_x < 0 {cat_x = 0;}
-        else if cat_x > width as i32 - cat_surface.width() as i32 {
-            cat_x = width as i32 - cat_surface.width() as i32
-        }
         let cat_rect = Rect::new(cat_x, cat_y, cat_surface.width(), cat_surface.height());
-        canvas.copy(&bg_img, None, None).unwrap();
-        canvas.copy(&cat_img, None, cat_rect).unwrap();
+        let apple_rect = Rect::new(apple_x, apple_y, apple_surface.width(), apple_surface.height());
+        if !game_over {
+            cat_x += cat_speed;
+            apple_y += apple_speed;
+            let dx = cat_x as f64 - apple_x as f64;
+            let dy = cat_y as f64 - apple_y as f64;
+            let distance = (dx * dx + dy * dy).sqrt();
+            time -= 1;
+            if apple_y > height as i32 {
+                apple_y = 0;
+                apple_x = random.random_range(0..=width as i32 - apple_surface.width() as i32);
+            }
+            if can_collide && distance < 60f64 {
+                score += 1;
+                apple_y = 0;
+                apple_x = random.random_range(0..=width as i32 - apple_surface.width() as i32);
+                can_collide = false;
+            }
+            if apple_y > 200 {can_collide = true;}
+            if time == 0 {game_over = true;}
+            canvas.copy(&bg_img, None, None).unwrap();
+            if !game_over {
+                canvas.copy(&cat_img, None, cat_rect).unwrap();
+                canvas.copy(&apple_img, None, apple_rect).unwrap();
+            }
+        }
         canvas.present();
         sleep(Duration::new(0, 1_000_000_000 / 60));
     }
